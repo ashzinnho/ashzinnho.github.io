662ea63e24d7a8bf29990af0597532daee3f5d37
ashzinnho <ashes.www.http@gmail.com>
2025-01-30 11:33:22 -0400

    poha2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  // Aqui você pode adicionar lógica ou variáveis se necessário.
+ 
 </script>
 
 <style>
@@ -49,20 +49,6 @@
 <div class="row g-0">
   <div class="col-md-4">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR682Bz_53t2CbkRQB739HFyx-M08HRJRK21g&s" class="img-fluid rounded-start" alt="...">
-  </div>
-  <div class="col-md-8">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
-</div>
-<div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-<div class="row g-0">
-  <div class="col-md-4">
-    <img src="..." class="img-fluid rounded-start" alt="...">
   </div>
   <div class="col-md-8">
     <div class="card-body">
