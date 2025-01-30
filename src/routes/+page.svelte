cdad668d218856cb66b71bf8b57e4c29d1fc8ca4
ashzinnho <ashes.www.http@gmail.com>
2025-01-30 11:28:15 -0400

    aaaaaaa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,78 @@
 <script>
-    // Aqui você pode adicionar lógica ou variáveis se necessário.
-  </script>
-  
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-    }
-  
-    nav {
-      background-color: #333;
-      display: flex;
-      justify-content: space-around;
-      padding: 10px;
-    }
-  
-    nav a {
-      color: white;
-      text-align: center;
-      padding: 14px 20px;
-      text-decoration: none;
-    }
-  
-    nav a:hover {
-      background-color: #ddd;
-      color: black;
-    }
-  </style>
+  // Aqui você pode adicionar lógica ou variáveis se necessário.
+</script>
 
-  <nav>
-    <a href="#abuble">Início</a>
-    <a href="#cinema">Sobre</a>
-    <a href="#coelho">Contato</a>
-  </nav>
-  <body>
-  <div id="home">
-    <h1>Bem-vindo ao Início!</h1>
-  </div>
-  
-  <div id="sobre">
-    <h1>Sobre nós</h1>
-  </div>
-  
-  <div id="contato">
-    <h1>Contato</h1>
-  </div>
+<style>
+  body {
+    background-color: #333
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  nav {
+    background-color: #333;
+    display: flex;
+    justify-content: space-around;
+    padding: 10px;
+  }
+
+  nav a {
+    color: white;
+    text-align: center;
+    padding: 14px 20px;
+    text-decoration: none;
+  }
+
+  nav a:hover {
+    background-color: #ddd;
+    color: black;
+  }
+</style>
+
+<nav>
+  <a href="#abuble">Início</a>
+  <a href="#cinema">Sobre</a>
+  <a href="#coelho">Contato</a>
+</nav>
+<body>
+<div id="home">
+  <h1>Bem-vindo ao Início!</h1>
+</div>
+
+<div id="sobre">
+  <h1>Sobre nós</h1>
+</div>
+
+<div id="contato">
+  <h1>Contato</h1>
+</div>
 </body>
+<div class="card mb-3" style="max-width: 540px;">
+<div class="row g-0">
+  <div class="col-md-4">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR682Bz_53t2CbkRQB739HFyx-M08HRJRK21g&s" class="img-fluid rounded-start" alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+</div>
+<div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+<div class="row g-0">
+  <div class="col-md-4">
+    <img src="..." class="img-fluid rounded-start" alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+</div>
